1b03d897980241cdbe2145513d3a39c513e0313c
Noel Pimentel <noelpimentel@berkeley.edu>
2015-11-09 20:11:29 -0800

    Merge code and .gitignored ds105

--- code/utils/merge_bold.py
+++ code/utils/merge_bold.py
@@ -1,8 +1,13 @@
 def merge_bold(subject):
-	directpath = os.path.realpath(subject)
-	runs = ['BOLD/task001_run'+ i + '/bold.nii.gz' for i in ['001','002','003','004','005','006','007','008','009','010','011','012']]
-	bolds_list = [nib.load(os.path.join(directpath, runs[i])).get_data() for i in range(len(runs))]
+	sub_path = os.path.realpath(subject)
+	runs_path = ['BOLD/task001_run'+ i + '/bold.nii.gz' for i in ['001','002','003','004','005','006','007','008','009','010','011’,’012’]]
+	bolds_list = [nib.load(os.path.join(sub_path, runs_path[i])).get_data() for i in range(len(runs_path))]
 	merged = np.concatenate(bolds_list, axis = 3)
 	return merged
 
-
+def merge_bold_5(subject):
+	sub_path = os.path.realpath(subject)
+	runs_path = ['BOLD/task001_run'+ i + '/bold.nii.gz' for i in ['001','002','003','004','005','006','007','008','009','010','011']]
+	bolds_list = [nib.load(os.path.join(sub_path, runs_path[i])).get_data() for i in range(len(runs_path))]
+	merged = np.concatenate(bolds_list, axis = 3)
+	return merged
