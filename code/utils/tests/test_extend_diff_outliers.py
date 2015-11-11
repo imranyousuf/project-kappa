10be2580ca1fe040db7b97aefecd6e67ed3961a8
Noel Pimentel <noelpimentel@berkeley.edu>
2015-11-10 21:38:37 -0800

    Fixing the tests

--- code/utils/tests/test_extend_diff_outliers.py
+++ code/utils/tests/test_extend_diff_outliers.py
@@ -7,7 +7,7 @@ Run with:
 
 import numpy as np
 
-from .. import extend_diff_outliers
+from .. import diagnostics as dg
 
 from nose.tools import assert_equal
 
@@ -17,17 +17,17 @@ from numpy.testing import assert_almost_equal, assert_array_equal
 def test_extend_diff_outliers():
     # Test function to extend difference outlier indices
     indices = np.array([3, 7, 12, 20])
-    assert_array_equal(extend_diff_outliers(indices),
+    assert_array_equal(dg.extend_diff_outliers(indices),
                        [3, 4, 7, 8, 12, 13, 20, 21])
 
 
 def test_sequential_input():
     indices = np.array([4, 5, 9, 10])
-    assert_array_equal(extend_diff_outliers(indices),
+    assert_array_equal(dg.extend_diff_outliers(indices),
                        [4, 5, 6, 9, 10, 11])
     indices = np.array([1, 2, 4, 5, 9, 10])
-    assert_array_equal(extend_diff_outliers(indices),
+    assert_array_equal(dg.extend_diff_outliers(indices),
                        [1, 2, 3, 4, 5, 6, 9, 10, 11])
     indices = np.array([3, 7, 8, 12, 20])
-    assert_array_equal(extend_diff_outliers(indices),
+    assert_array_equal(dg.extend_diff_outliers(indices),
                        [3, 4, 7, 8, 9, 12, 13, 20, 21])
