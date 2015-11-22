f0307eaae9c8ffe088403d2b98c5c681542c11f9
changzheng1993 <changzheng1993@gmail.com>
2015-11-22 15:36:31 -0800

    add a function to open BOLD file

---
+++ code/utils/open_bold.py
@@ -0,0 +1,8 @@
+def open_bold (subject):
+	""" Find all task_run files from a subject BOLD """ 
+
+	sub_path = os.path.realpath(subject)
+	sub_path_BOLD = sub_path + '/BOLD'
+	task_run = [ i for i in os.listdir(sub_path_BOLD)]
+
+	return task_run[1:]
