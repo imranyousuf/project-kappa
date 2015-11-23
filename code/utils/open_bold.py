4a2406a7f6de7a7a281b17456e1d707ffafe21a3
Noel Pimentel <noelpimentel@berkeley.edu>
2015-11-22 16:09:56 -0800

    Fixed hidden folder issue

--- code/utils/open_bold.py
+++ code/utils/open_bold.py
@@ -3,6 +3,6 @@ def open_bold (subject):
 
 	sub_path = os.path.realpath(subject)
 	sub_path_BOLD = sub_path + '/BOLD'
-	task_run = [ i for i in os.listdir(sub_path_BOLD)]
+	task_run = [ i for i in os.listdir(sub_path_BOLD) if not (i.startswith('.'))]
 
-	return task_run[1:]
+	return task_run
