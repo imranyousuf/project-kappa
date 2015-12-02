8a1cae7329b736201f63bb1f9a177565180e364d
changzheng1993 <changzheng1993@gmail.com>
2015-12-01 17:31:50 -0800

    fitxit

--- code/utils/knn.py
+++ code/utils/knn.py
@@ -2,74 +2,34 @@
 
 from numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
 
 
+def knn(X_train, X_test, y_train, y_test, k = 5):
+    """ This function returns test error by using k-nearest neighbors
+        
+        Parameters
+        ----------
+        X_train: 2D array
+            Training feature data set
+        X_test: 2D array
+            Test feature data set
+        y_train: 1D array
+            Training response variable
+        y_test: 1D array
+            Test response variable
+        k: int or float (optional)
+            the number of sample. The default value is 5.
+        
+        Returns
+        -------
+            test_error: float
+        test error
+        
+        """
+        knn = KNeighborsClassifier(n_neighbors = k)
+        knn.fit(X_train,y_train)
+        predicted = knn.predict(X_test)
+        test_error = 1 - np.mean(y_test == predicted)
+                            
+        return test_errortest_error
 
-X = data
-y = target
-
-# Train on 70% of the data, test on the remaining 30%
-test_size = 0.3 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-
-# k = 1
-# Create the model for k =1 
-knn = KNeighborsClassifier(n_neighbors = 1)
-
-#fit the model
-knn.fit(X_train,y_train)
-
-# Predict the response variable
-predicted = knn.predict(X_test)
-
-# Calculate the test error
-test_error = 1 - np.mean(y_test == predicted)
-
-test_error
-
-# Get more detail information
-confusion_matrix(y_test, predicted)
-print(classification_report(y_test, predicted))
-
-
-# k = 5
-# Create the model for k = 5 
-knn = KNeighborsClassifier(n_neighbors = 5)
-
-#fit the model
-knn.fit(X_train,y_train)
-
-# Predict the response variable
-predicted = knn.predict(X_test)
-
-# Calculate the test error
-test_error = 1 - np.mean(y_test == predicted)
-
-test_error
-
-# Get more detail information
-confusion_matrix(y_test, predicted)
-print(classification_report(y_test, predicted))
-
-
-# k = 10
-# Create the model for k = 10 
-knn = KNeighborsClassifier(n_neighbors = 10)
-
-#fit the model
-knn.fit(X_train,y_train)
-
-# Predict the response variable
-predicted = knn.predict(X_test)
-
-# Calculate the test error
-test_error = 1 - np.mean(y_test == predicted)
-
-test_error
-
-# Get more detail information
-confusion_matrix(y_test, predicted)
-print(classification_report(y_test, predicted))
