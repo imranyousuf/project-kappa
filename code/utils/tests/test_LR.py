fe9d4107e4433647fa5af594444f608893e1c68d
iyousuf <iyousuf@Ebay.com>
2015-12-03 04:34:42 -0800

    added and removed some dependcies

--- code/utils/tests/test_LR.py
+++ code/utils/tests/test_LR.py
@@ -1,4 +1,3 @@
 # This file is for Logistic regression
 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
--- code/utils/tests/test_high_res.py
+++ code/utils/tests/test_high_res.py
@@ -1,6 +1,3 @@
 import numpy as np
 import nibabel as nib
 import os
-from list_cond import *
-from path_bold import *
-from cond_path import *
