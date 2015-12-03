8af3b473558af6ceaa014dd06cb61c1d671737a4
changzheng1993 <changzheng1993@gmail.com>
2015-12-02 20:30:38 -0800

    fixed some codes

--- code/utils/LDA.py
+++ code/utils/LDA.py
@@ -6,27 +6,28 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 def lda(X_train, X_test, y_train, y_test):
     """ This function returns test_error by using linear discriminant analysis
         
-        Parameters
-        ----------
-        X_train: 2D array
-            Training feature data set
-        X_test: 2D array
-            Test feature data set
-        y_train: 1D array
-            Training response variable
-        y_test: 1D array
-            Test response variable
+    Parameters
+    ----------
+    X_train: 2D array
+        Training feature data set
+    X_test: 2D array
+        Test feature data set
+    y_train: 1D array
+        Training response variable
+    y_test: 1D array
+        Test response variable
         
-        Returns
-        -------
-        test_error: float
-            test error
+    Returns
+    -------
+    test_error: float
+        test error
         
-        """
-        clf = LinearDiscriminantAnalysis()
-        clf.fit(X_train, y_train)
-        predicted = clf.predict(X_test)
-        test_error = 1 - np.mean(y_test == predicted)
+    """
+    
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test)
+    test_error = 1 - np.mean(y_test == predicted)
                             
-        return test_error
+    return test_error
 
--- code/utils/knn.py
+++ code/utils/knn.py
@@ -1,35 +1,36 @@
 # This file is for k-nearest neighbors
 
-from numpy as np
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
 
 def knn(X_train, X_test, y_train, y_test, k = 5):
     """ This function returns test error by using k-nearest neighbors
         
-        Parameters
-        ----------
-        X_train: 2D array
-            Training feature data set
-        X_test: 2D array
-            Test feature data set
-        y_train: 1D array
-            Training response variable
-        y_test: 1D array
-            Test response variable
-        k: int or float (optional)
-            the number of sample. The default value is 5.
+    Parameters
+    ----------
+    X_train: 2D array
+        Training feature data set
+    X_test: 2D array
+        Test feature data set
+    y_train: 1D array
+        Training response variable
+    y_test: 1D array
+        Test response variable
+    k: int or float (optional)
+        the number of sample. The default value is 5.
         
-        Returns
-        -------
-            test_error: float
+    Returns
+    -------
+    test_error: float
         test error
         
-        """
-        knn = KNeighborsClassifier(n_neighbors = k)
-        knn.fit(X_train,y_train)
-        predicted = knn.predict(X_test)
-        test_error = 1 - np.mean(y_test == predicted)
+    """
+    
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(X_train,y_train)
+    predicted = knn.predict(X_test)
+    test_error = 1 - np.mean(y_test == predicted)
                             
-        return test_errortest_error
+    return test_error
 
