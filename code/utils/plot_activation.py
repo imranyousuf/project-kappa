9206091aab42ce3a0015e0214de3ff72aee655bd
Noel Pimentel <noelpimentel@berkeley.edu>
2015-12-12 14:59:55 -0800

    @giancarloescobar let me know if it works

--- code/utils/plot_activation.py
+++ code/utils/plot_activation.py
@@ -1,17 +1,21 @@
 from matplotlib import colors
 import numpy as np
+import os
 import matplotlib.pyplot as plt
-def plot_act(betaVols, meanData,mask,condition):
+def plot_act(subject, betas_vols, mean_data, mask, condition ):
         
-    meanData[~mask]=np.nan 
-    betaVols[~mask]=np.nan
-    nice_cmap_values = np.loadtxt('actc.txt')
-    nice_cmap = colors.ListedColormap(nice_cmap_values,'actc')
+    mean_data[~mask]=np.nan 
+    betas_vols[~mask]=np.nan
+    #nice_cmap_values = np.loadtxt('actc.txt')
+    #nice_cmap = colors.ListedColormap(nice_cmap_values,'actc')
 
-    for i in range(meanData.shape[-1]):
-        plt.imshow(meanData[:,:,i],cmap='gray',alpha=0.5,interpolation='nearest')
-        plt.imshow(betaVols[:,:,i,condition-1],cmap=nice_cmap, alpha=0.5, interpolation ='nearest')
-        plt.savefig('activation'+str(i)+'_'+str(condition-1)+'.png')
+    activation_path = str(subject) + '_c' + str(condition) + '_act'
+    os.makedirs(activation_path)
+
+    for i in range(mean_data.shape[-1]):
+        plt.imshow(mean_data[:,:,i],cmap='gray',alpha=0.5,interpolation='nearest')
+        plt.imshow(betas_vols[:,:,i,condition-1], alpha=0.5, interpolation ='nearest')
+        plt.savefig(activation_path + '/activation'+str(i)+'_'+str(condition-1)+'.png')
         plt.close()
 
 
